89fb78c77ffe694e0f519a9cc1706f3df442ea1f
KankamYeboah <kankamyeboah1@gmail.com>
2023-08-18 01:13:33 +0000

    Folder Tree Clean Up

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const API_KEY = "3835df8c9b0f9d1df10ac91adf406d09";
+export const API_KEY = ""; //include your url api key for the TMDB API.
 export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
